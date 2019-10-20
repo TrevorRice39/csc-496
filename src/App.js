50d67e5fe7e5cba1011e980e973d7cf19633ff1f
Trevor Rice <trevor_rice39@mymail.eku.edu>
2019-10-20 13:21:01 -0400

    base site created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './recipes_logo.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>CSC 496 Recipes</div>
+        
       </header>
+      <body></body>
+      <footer>
+        Trevor Rice
+        <br></br>
+        CSC 499
+        <br></br>
+        trevor_rice39@mymail.eku
+        <a href="https:github.com/TrevorRice39">Github</a>
+      </footer>
     </div>
   );
 }
